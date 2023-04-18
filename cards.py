d1e68bed81909af521ce00c3dcc97c5dd1d0f2ee
JuliusStein <juliusastein@gmail.com>
2023-04-18 03:01:40 -0400

    cards working!

--- cards.py
+++ cards.py
@@ -2,14 +2,22 @@ import random
 global playerDeck
 
 class Card:
-    def __init__(self, cardCategory, cardDescription, leftText = "No", rightText = "Yes", cardImage = "",  cardCost=0, cardHappiness=0, cardHealth=0, cardSavings=0):
+    def __init__(self, cardCategory, cardDescription, leftText = "Left", rightText = "Right", 
+                 cardImage = "assets\cardImages\defaultCardImage.png",
+                 leftCost=0, rightCost=0,leftHappiness=0,rightHappiness=0, 
+                 leftHealth=0, rightHealth=0, leftSavings=0, rightSavings=0):
+        
         self.image = cardImage
         self.description = cardDescription
-        self.cost = cardCost
-        self.happiness = cardHappiness
-        self.health = cardHealth
-        self.savings = cardSavings
         self.category = cardCategory
+        self.leftCost = leftCost
+        self.rightCost = rightCost
+        self.leftHappiness = leftHappiness
+        self.rightHappiness = rightHappiness
+        self.leftHealth = leftHealth
+        self.rightHealth = rightHealth
+        self.leftSavings = leftSavings
+        self.rightSavings = rightSavings
         self.leftText = leftText
         self.rightText = rightText
 
@@ -36,11 +44,21 @@ class Deck:
     def getCard(self, index):
         return self.cards[index]
     
-playerDeck = Deck("Main Deck")
-card1 = Card("Category BB", "Description", "Left", "Right", "Image", 1, 2, 3, 4)
-playerDeck.addCard(card1)
+    def loadCards(self, pathToCSV):
+        import csv
+        global playerDeck
+        with open(pathToCSV) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count != 0:
+                    card = Card(row[0], row[1], row[2], row[3], "assets\cardImages\defaultCardImage.png", float(row[4]), float(row[5]), float(row[6]), float(row[7]), float(row[8]), float(row[9]), float(row[10]), float(row[11]))
+                    playerDeck.addCard(card)
+                line_count += 1
 
-def addRelationshipCards():
-    global playerDeck
-    card2 = Card("Category", "Description", "Left", "Right", "Image", 4, 3, 2, 1)
-    playerDeck.addCard(card2)
+                #  For Debuging Decks:
+                #print(f'Card #{line_count}: {row[0]} {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]} {row[7]} {row[8]} {row[9]} {row[10]} {row[11]}')
+
+        
+playerDeck = Deck("Main Deck")
+playerDeck.loadCards('cards_v1.csv')
--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
-from cards import Card, Deck, playerDeck, addRelationshipCards
+from cards import Card, Deck, playerDeck
 
 #----------------- Variables -------------------
-click = 0
-amount = 1
+global cash, health, happiness, age, retirementAge, savings, savingsGoal, morgage, car, student_loan, credit_card, married, rent_cost, morgage_cost, car_cost, student_loan_cost, credit_card_cost, num_kids
 
-cash = 10000
+cash = 1000
 health = 100
 happiness = 100
 
@@ -40,36 +39,40 @@ def resizeImage(file, width, height):
 
 from random import randint
 def draw():
-  roll = randint(1, 5)
-  if roll <= 1:
-    print("You got a good card!")
-  elif roll <= 2:
-    print("You got no card.")
-  elif roll <= 3:
-    print("You got no card.")
-  elif roll <= 4:
-    print("You got no card.")
-  elif roll <= 5:
-    print("You got a bad card!") 
+  global playerDeck
+  playerDeck.shuffle()
+  card = playerDeck.drawCard()
+  return card
+  
+global currentCard
+def updateCard():
+  global window, cardText, cardImage, cardImageLabel, currentCard, leftText, rightText
+  currentCard = draw()
+  cardText.configure(text=currentCard.description)
+  leftText.configure(text=currentCard.leftText)
+  rightText.configure(text=currentCard.rightText)
+  cardImage = resizeImage(currentCard.image, 320, 270)
+  cardImageLabel = Label(window, image=cardImage, bg='#565040')
+  cardImageLabel.place(x=450, y=225)
 
-def clicked():
-  draw()
-
-  global click
-  global amount
-  click += amount
-  lbl.configure(text=click)
+def applyCard(card, side):
+  if side == "LEFT":
+    return
+  elif side == "RIGHT":
+    return
 
 def clickedLeft():
-  global amount
-  amount -= 1
+  global currentCard
+  updateCard()
+  applyCard(currentCard, "LEFT")
 
 def clickedRight():
-  global amount
-  amount += 1
+  global currentCard
+  updateCard()
+  applyCard(currentCard, "RIGHT")
 
 #----------------- Window -------------------
-
+global window
 window = Tk()
 window.title("Money v Happy")
 window.geometry('1200x900')
@@ -77,16 +80,17 @@ window.geometry('1200x900')
 
 #--------------- Header Bar ---------------------
 headerImage = resizeImage('assets/header_1200x150.png', 1200, 150)
-lbl = Label(window, text="0", image=headerImage)
+lbl = Label(window, text="", image=headerImage)
 lbl.place(x=0, y=0)
 
+global bear, bearButton, cashLabel, cashButton, happinessLabel, happinessBar, healthLabel, healthBar, ageLabel, ageBar, savingsLabel, savingsBar, savingsGoalLabel, savingsGoalBar
 bear = resizeImage("assets/bear_happy_150X150.png", 125, 125)
-bearButton = Button(window, image=bear, command=clicked)
+bearButton = Button(window, image=bear, command=updateCard)
 bearButton.place(x=550, y=10)
 
 cashLabel = Label(window, text="Cash", font=("Arial", 12), bg='#565040', fg='white')
 cashLabel.place(x=215, y=100)
-cashButton = Button(window, text="$"+str(cash), command=clicked, font=("Arial", 10), bg='#565040', fg='white')
+cashButton = Button(window, text="$"+str(cash), command=updateCard, font=("Arial", 10), bg='#565040', fg='white')
 cashButton.place(x=270, y=100)
 
 happinessLabel = Label(window, text="Happiness", bg='#565040', fg='white', font=("Arial", 10))
@@ -115,14 +119,14 @@ savingsGoalBar['variable'] = savings
 
 savingsLabel = Label(window, text="Savings", font=("Arial", 12), bg='#565040', fg='white')
 savingsLabel.place(x=770, y=100)
-savingsButton = Button(window, text="$"+str(savings), command=clicked, font=("Arial", 10), bg='#565040', fg='white')
+savingsButton = Button(window, text="$"+str(savings), command=updateCard, font=("Arial", 10), bg='#565040', fg='white')
 savingsButton.place(x=845, y=100)
 
 ageLabel = Label(window, text="Age", bg='#565040', fg='white', font=("Arial", 10))
 ageLabel.place(x=1010, y=25)
 ageBar = ttk.Progressbar(window, orient=VERTICAL, length=120, mode='determinate')
 ageBar.place(x=1050, y=5)
-ageBar['value'] = age
+ageBar['value'] = age - 25
 ageBar['maximum'] = retirementAge
 ageBar['variable'] = age
 
@@ -147,38 +151,54 @@ cardOutlineLabel = Label(window, image=img, bg='#565040')
 cardOutlineLabel.place(x=425, y=200)
 
 #Card Image
-cardImage = resizeImage('assets/blankCard.png', 320, 270)
+global cardImage, cardImageLabel
+cardImage = resizeImage('assets/cardImages/defaultCardImage.png', 320, 270)
 cardImageLabel = Label(window, image=cardImage, bg='#565040')
 cardImageLabel.place(x=450, y=225)
 
 #Card Text
+global cardText
 cardText = Label(window, text="This is the card text", bg='#565040', fg='white', font=("Arial", 10), wraplength=300, justify=CENTER)
 cardText.place(x=470, y=500)
 
 #Left Text
+global leftText
 leftText = Label(window, text="Left", bg='#565040', fg='white', font=("Arial", 10),width=10)
 leftText.place(x=250, y=425)
 
 #Right Text
+global rightText
 rightText = Label(window, text="Right", bg='#565040', fg='white', font=("Arial", 10),width=10)
 rightText.place(x=900, y=425)
 
 #Card Left Button
+global cardLeftButton
 leftButton = resizeImage('assets/leftButton.png', 79, 79)
 cardLeftButton = Button(window, command=clickedLeft, bg='#565040', fg='white', image=leftButton)
 cardLeftButton.place(x=250, y=450)
 
 #Card Right Button
+global cardRightButton
 rightButton = resizeImage('assets/rightButton.png', 79, 79)
 cardRightButton = Button(window, command=clickedRight, bg='#565040', fg='white', image=rightButton)
 cardRightButton.place(x=900, y=450)
 
 #------------------------------------
 
-cardText.configure(text="This is the card text "*30)
+#cardText.configure(text="This is the card text "*30)
 
+global deck
 deck = playerDeck
 card = deck.drawCard()
-print(card.category)
+
+draw()
 
 window.mainloop()
+
+
+
+if __name__ == "__main__":
+  deck = playerDeck
+  card = deck.drawCard()
+  
+  window.mainloop()
