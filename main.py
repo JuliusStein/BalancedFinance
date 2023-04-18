8c52035fdac72a0b1a63238a0064a13bd7fd5dea
JuliusStein <juliusastein@gmail.com>
2023-04-18 00:49:03 -0400

    working UI

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+#----------------- Imports -------------------
 from tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
 
+#----------------- Variables -------------------
 click = 0
 amount = 1
-cost1 = 50
 
 cash = 10000
 health = 100
@@ -27,18 +29,13 @@ student_loan_cost = 0
 credit_card_cost = 0
 
 num_kids = 0
-
-#--------------------- Window Defn -------------------------
-window = Tk()
-window.title("Money v Happy")
-window.geometry('1200x900')
-
-background_image = PhotoImage(file="hillandale.png")
-background_label = Label(window, image=background_image)
-background_label.place(x=475, y=175, relwidth=0.5, relheight=0.5)
-
-
 #----------------- Functions -------------------
+
+def resizeImage(file, width, height):
+  image = Image.open(file)
+  resize_image = image.resize((width, height))
+  img = ImageTk.PhotoImage(resize_image)
+  return img
 
 from random import randint
 def rollCard():
@@ -62,23 +59,36 @@ def clicked():
   click += amount
   lbl.configure(text=click)
 
+def clickedLeft():
+  global amount
+  amount -= 1
 
+def clickedRight():
+  global amount
+  amount += 1
+
+#----------------- Window -------------------
+
+window = Tk()
+window.title("Money v Happy")
+window.geometry('1200x900')
+#window.wm_attributes('-transparentcolor','grey')
 
 #--------------- Header Bar ---------------------
-lbl = Label(window, text="0")
-lbl.place(x=10, y=10)
+headerImage = resizeImage('assets/header_1200x150.png', 1200, 150)
+lbl = Label(window, text="0", image=headerImage)
+lbl.place(x=0, y=0)
 
-current = PhotoImage(file='current.png')
-current.configure(width=120, height=120)
-btn = Button(window, image=current, command=clicked)
-btn.place(x=525, y=0)
+bear = resizeImage("assets/bear_happy_150X150.png", 125, 125)
+bearButton = Button(window, image=bear, command=clicked)
+bearButton.place(x=550, y=10)
 
-cashLabel = Label(window, text="Cash")
-cashLabel.place(x=150, y=100)
-cashButton = Button(window, text="$"+str(cash), command=clicked)
-cashButton.place(x=225, y=100)
+cashLabel = Label(window, text="Cash", font=("Arial", 12), bg='#565040', fg='white')
+cashLabel.place(x=215, y=100)
+cashButton = Button(window, text="$"+str(cash), command=clicked, font=("Arial", 10), bg='#565040', fg='white')
+cashButton.place(x=270, y=100)
 
-happinessLabel = Label(window, text="Happiness")
+happinessLabel = Label(window, text="Happiness", bg='#565040', fg='white', font=("Arial", 10))
 happinessLabel.place(x=50, y=25)
 happinessBar = ttk.Progressbar(window, orient=HORIZONTAL, length=200, mode='determinate')
 happinessBar.place(x=50, y=50)
@@ -86,7 +96,7 @@ happinessBar['value'] = happiness
 happinessBar['maximum'] = 100
 happinessBar['variable'] = happiness
 
-healthLabel = Label(window, text="Health")
+healthLabel = Label(window, text="Health", bg='#565040', fg='white', font=("Arial", 10))
 healthLabel.place(x=275, y=25)
 healthBar = ttk.Progressbar(window, orient=HORIZONTAL, length=200, mode='determinate')
 healthBar.place(x=275, y=50)
@@ -94,30 +104,76 @@ healthBar['value'] = health
 healthBar['maximum'] = 100
 healthBar['variable'] = health
 
-savingsLabel = Label(window, text="Savings")
-savingsLabel.place(x=700, y=25)
-savingsBar = ttk.Progressbar(window, orient=HORIZONTAL, length=200, mode='determinate')
-savingsBar.place(x=700, y=50)
-savingsBar['value'] = savings
-savingsBar['maximum'] = savingsGoal
-savingsBar['variable'] = savings
+savingsGoalLabel = Label(window, text="Savings Goal", bg='#565040', fg='white', font=("Arial", 10))
+savingsGoalLabel.place(x=770, y=25)
+savingsGoalBar = ttk.Progressbar(window, orient=HORIZONTAL, length=200, mode='determinate')
+savingsGoalBar.place(x=770, y=50)
+savingsGoalBar['value'] = savings
+savingsGoalBar['maximum'] = savingsGoal
+savingsGoalBar['variable'] = savings
 
-ageLabel = Label(window, text="Age")
-ageLabel.place(x=950, y=25)
-ageBar = ttk.Progressbar(window, orient=HORIZONTAL, length=200, mode='determinate')
-ageBar.place(x=950, y=50)
+savingsLabel = Label(window, text="Savings", font=("Arial", 12), bg='#565040', fg='white')
+savingsLabel.place(x=770, y=100)
+savingsButton = Button(window, text="$"+str(savings), command=clicked, font=("Arial", 10), bg='#565040', fg='white')
+savingsButton.place(x=845, y=100)
+
+ageLabel = Label(window, text="Age", bg='#565040', fg='white', font=("Arial", 10))
+ageLabel.place(x=1010, y=25)
+ageBar = ttk.Progressbar(window, orient=VERTICAL, length=120, mode='determinate')
+ageBar.place(x=1050, y=5)
 ageBar['value'] = age
 ageBar['maximum'] = retirementAge
 ageBar['variable'] = age
 
-#------------------------------------
+ageNumberLabel = Label(window, text=str(age), font=("Arial", 10), bg='#565040', fg='white')
+ageNumberLabel.place(x=1051, y=125)
+retirementNumberLabel = Label(window, text=str(retirementAge), font=("Arial", 10), bg='#565040', fg='white')
+retirementNumberLabel.place(x=1075, y=10)
 
+#------------------------------------
 
 ttk.Separator(window, orient=HORIZONTAL).place(x=0, y=150, width=1200)
+
+#------------- Game Space -----------------------
+#Background
+backgroundImage = resizeImage('assets/background_1200x750.png', 1200, 750)
+backgroundLabel = Label(window, image=backgroundImage)
+backgroundLabel.place(x=0, y=150)
+
+#Card Outline
+img = resizeImage('assets/blankCard.png',370, 550)
+cardOutlineLabel = Label(window, image=img, bg='#565040')
+cardOutlineLabel.place(x=425, y=200)
+
+#Card Image
+cardImage = resizeImage('assets/blankCard.png', 320, 270)
+cardImageLabel = Label(window, image=cardImage, bg='#565040')
+cardImageLabel.place(x=450, y=225)
+
+#Card Text
+cardText = Label(window, text="This is the card text", bg='#565040', fg='white', font=("Arial", 10), wraplength=300, justify=CENTER)
+cardText.place(x=470, y=500)
+
+#Left Text
+leftText = Label(window, text="Left", bg='#565040', fg='white', font=("Arial", 10),width=10)
+leftText.place(x=250, y=425)
+
+#Right Text
+rightText = Label(window, text="Right", bg='#565040', fg='white', font=("Arial", 10),width=10)
+rightText.place(x=900, y=425)
+
+#Card Left Button
+leftButton = resizeImage('assets/leftButton.png', 79, 79)
+cardLeftButton = Button(window, command=clickedLeft, bg='#565040', fg='white', image=leftButton)
+cardLeftButton.place(x=250, y=450)
+
+#Card Right Button
+rightButton = resizeImage('assets/rightButton.png', 79, 79)
+cardRightButton = Button(window, command=clickedRight, bg='#565040', fg='white', image=rightButton)
+cardRightButton.place(x=900, y=450)
+
 #------------------------------------
 
-#Game Space
-
-
+cardText.configure(text="This is the card text "*30)
 
 window.mainloop()
